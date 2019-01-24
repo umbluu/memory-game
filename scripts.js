8a2a340d3491fe2e46d60bb1afaaf6db18c84f1b
umbluu <andrep615@hotmail.com>
2019-01-24 16:34:12 +0200

    Final touches

--- scripts.js
+++ scripts.js
@@ -7,6 +7,8 @@ let firstCard, secondCard;
 
 function flipCard() {
     if(lockBoard) return;
+    if(this === firstCard) return;
+
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -16,7 +18,6 @@ function flipCard() {
     }
 
     secondCard = this;
-    hasFlippedCard = false;
 
     checkForMatch();
 }
@@ -33,19 +34,34 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
     lockBoard = true;
+    
 
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
 
-        lockBoard = false;
+        resetBoard();
     }, 1500);
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+    
+}
 
-console.log(cards);
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
