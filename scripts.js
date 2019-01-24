2c42af375cc94c1ba803fe6709cec9e81c2b3504
umbluu <andrep615@hotmail.com>
2019-01-24 16:16:13 +0200

    Implementing 'Lock Board' feature

--- scripts.js
+++ scripts.js
@@ -2,15 +2,48 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if(lockBoard) return;
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
+        return;
     }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if(firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockBoard = false;
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
