404e34c4f2ebc11ce95617d64a8fc481d13b55cb
umbluu <andrep615@hotmail.com>
2019-01-24 16:05:25 +0200

    Adding logic

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// scripts.js
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    }
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+console.log(cards);
